241d6e0e2cb1b9b434cd31bd0c3e76e872e0f962
hidetomo117 <hidetomo117@gmail.com>
2020-02-01 18:38:32 +0900

    implement UI

--- Swift_CryptoKit/UI/ViewController/Main/MainViewController.swift
+++ Swift_CryptoKit/UI/ViewController/Main/MainViewController.swift
@@ -7,40 +7,87 @@ import UIKit
 
 final class MainViewController: UIViewController {
     
+    // MARK: - IBOutlet
+    
+    @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
+    // MARK: - View  life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setup()
     }
+    
+    // MARK: - IBAction
+    
+    @IBAction private func tappedEncryptButton(_ sender: UIButton) {
+        doEncrypt()
+    }
+    
+    @IBAction private func tappedDecryptButton(_ sender: UIButton) {
+        doDecrypt()
+    }
 }
 
 private extension MainViewController {
     
+    /// 初期処理
     private func setup() {
-        
-        // 書き込むパスの取得
+        print("You can open easily with command -> command + shift + G: \(getFilePath())")
+        setupLayout()
+    }
+    
+    /// レイアウトの設定
+    private func setupLayout() {
+        resultLabel.layer.borderWidth = 1
+        descriptionLabel.layer.borderWidth = 1
+        resultLabel.layer.borderColor = UIColor.black.cgColor
+        descriptionLabel.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    /// 暗号化を実行
+    private func doEncrypt() {
+        let path = getFilePath()
+        guard
+            let data = getPathData(path: path),
+            CryptManager.shared.writeEncryptedData(to: path, data: data) else {
+                updateLabelText(result: "Failure!", description: "Failed to write encrypted data.")
+                return
+        }
+        updateLabelText(result: "Succeed!", description: " Succeed to write encrypted data.")
+    }
+    
+    /// 復号を実行
+    private func doDecrypt() {
+        let path = getFilePath()
+        guard
+            let data = FileManager.default.contents(atPath: path),
+            CryptManager.shared.writeDecryptedData(to: path, data: data)else {
+                updateLabelText(result: "Failure!", description: "Failed to write decrypted data.")
+                return
+        }
+        updateLabelText(result: "Succeed!", description: " Succeed to write decrypted data.")
+    }
+    
+    /// 書き込むパスを取得
+    private func getFilePath() -> String {
         let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-        let filePath = "\(documentsPath)/Sample.text"
-        
-        print("command + shift + G: \(filePath)")
-        
-        // ファイルをデータ型で取得(デバックのため、"filePath"にあらかじめテスト用のファイル"Sample.text"を配置)
-        guard let encryptData = FileManager.default.contents(atPath: filePath) else {
-            return
+        return "\(documentsPath)/Sample.text"
+    }
+    
+    // 指定したパスのデータを取得
+    private func getPathData(path: String) -> Data? {
+        guard let encryptData = FileManager.default.contents(atPath: path) else {
+            return nil
         }
-        
-        //  暗号化
-        if !CryptManager.shared.writeEncryptedData(to: filePath, data: encryptData) {
-            print("failed to write encrypted data.")
-        }
-        
-        guard let decryptData = FileManager.default.contents(atPath: filePath) else {
-            return
-        }
-        
-        // 復号
-        if !CryptManager.shared.writeDecryptedData(to: filePath, data: decryptData) {
-            print("failed to write decrypted data.")
-        }
+        return encryptData
+    }
+    
+    /// 表示文言を変更
+    private func updateLabelText(result: String, description: String) {
+        resultLabel.text = result
+        descriptionLabel.text = description
     }
 }
